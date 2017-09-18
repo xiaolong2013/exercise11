45e8383295ace50b897c1d89712ee1770fcd3382
式燕 <wenjian.ywj@alibaba-inc.com>
2017-09-19 00:49:03 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# webpack 练习
+
+### 要求
+
+* `npm run build` 时候生成代码有 css，并且代码js代码被压缩
+* `npm run dev` 时候可以不生成 css（或者说不使用`extract-text-webpack-plugin`），js代码不被压缩
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+var path = require('path')
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+// 这个在 npm run dev 和 npm run build 时候是不同的
+var TARGET = process.env.npm_lifecycle_event
+var APP_PATH = path.join(__dirname, '/src')
